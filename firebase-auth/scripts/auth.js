49fcdc877f8e8bb9b4c4429b7119d95da09e5f0f
louiscklaw <louiscklaw@gmail.com>
2020-05-25 11:56:14 +0800

    tidy up script,

--- firebase-auth/scripts/auth.js
+++ firebase-auth/scripts/auth.js
@@ -2,43 +2,43 @@
 
 
 
-const signupForm = document.querySelector('#signup-form');
+const signupForm = document.querySelector( '#signup-form' );
 
-signupForm.addEventListener('submit', (e) => {
+signupForm.addEventListener( 'submit', ( e ) => {
   e.preventDefault();
 
-  const email = signupForm['signup-email'].value;
-  const password = signupForm['signup-password'].value;
+  const email = signupForm[ 'signup-email' ].value;
+  const password = signupForm[ 'signup-password' ].value;
 
-  auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    const modal = document.querySelector('#modal-signup');
-    M.Modal.getInstance(modal).close();
+  auth.createUserWithEmailAndPassword( email, password ).then( cred => {
+    const modal = document.querySelector( '#modal-signup' );
+    M.Modal.getInstance( modal ).close();
     signupForm.reset();
-  });
-});
+  } );
+} );
 
 // logout
-const logout = document.querySelector('#logout')
-logout.addEventListener('click', (e) => {
+const logout = document.querySelector( '#logout' )
+logout.addEventListener( 'click', ( e ) => {
   e.preventDefault();
-  auth.signOut().then(() => {
-    console.log('user signed out')
-  });
-})
+  auth.signOut().then( () => {
+    console.log( 'user signed out' )
+  } );
+} )
 
 // login
-const loginForm = document.querySelector('#login-form')
-loginForm.addEventListener('submit', (e) => {
+const loginForm = document.querySelector( '#login-form' )
+loginForm.addEventListener( 'submit', ( e ) => {
   e.preventDefault();
 
-  const email = loginForm['login-email'].value;
-  const password = loginForm['login-password'].value;
+  const email = loginForm[ 'login-email' ].value;
+  const password = loginForm[ 'login-password' ].value;
 
-  auth.signInWithEmailAndPassword(email, password).then((cred) => {
-    console.log(cred.user)
+  auth.signInWithEmailAndPassword( email, password ).then( ( cred ) => {
+    console.log( cred.user )
 
-    const modal = document.querySelector('#modal-login')
-    M.Modal.getInstance(modal).close()
+    const modal = document.querySelector( '#modal-login' )
+    M.Modal.getInstance( modal ).close()
     loginForm.reset()
-  })
+  } )
 } )
