ec6cae5c25a5c4a605875a264731def15b1e6445
louiscklaw <louiscklaw@gmail.com>
2020-05-25 09:29:48 +0800

    adding delete data,

--- html-firebase-firestore-tryout/app.js
+++ html-firebase-firestore-tryout/app.js
@@ -6,17 +6,30 @@ function renderCafe(doc){
   let li = document.createElement('li');
   let name = document.createElement('span');
   let city = document.createElement('span');
+  let cross = document.createElement('div');
+
   li.setAttribute('data-id', doc.id)
+
 
   name.textContent = doc.data().name;
   city.textContent = doc.data().city;
+  cross.textContent = 'x';
 
   li.appendChild(name);
   li.appendChild(city);
+  li.appendChild(cross);
 
   cafeList.appendChild(li);
 
   console.log(li)
+
+  cross.addEventListener('click', (e)=>{
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute('data-id');
+
+    db.collection('cafes').doc(id).delete();
+    console.log('delete using id')
+  } )
 }
 
 // getting data
@@ -24,6 +37,7 @@ db.collection('cafes').get().then((snapshot) => {
   snapshot.docs.forEach((doc) => {
     renderCafe(doc)
   })
+
 })
 
 // saving data
@@ -33,4 +47,6 @@ form.addEventListener('submit', (e) => {
     name: form.name.value,
     city: form.city.value
   })
+  form.name.value = '';
+  form.city.value = '';
 })
