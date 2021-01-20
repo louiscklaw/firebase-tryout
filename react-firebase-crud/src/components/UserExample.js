996f010bd3b80e42550e4bc7c1383f777ff8d070
louiscklaw <louiscklaw@gmail.com>
2021-01-20 11:36:39 +0800

    update,

--- react-firebase-crud/src/components/UserExample.js
+++ react-firebase-crud/src/components/UserExample.js
@@ -1,50 +1,92 @@
 import React from "react";
 
 import { addUser, processAddUser } from "src/components/modals/user";
-
-function handleAddUser(e) {
-  e.preventDefault();
-  let target_form = document.forms["add-user-form"];
-  let username = target_form.username.value;
-  let password = target_form.password.value;
-  processAddUser(username, password)
-    .then(() => {
-      alert("add user done");
-    })
-    .catch((err) => {
-      alert(err);
-    });
-}
-
-function AddUser() {
-  return (
-    <>
-      <form name="add-user-form">
-        <div>
-          username:
-          <input type="text" name="username" id="username" value="testuser" />
-        </div>
-        <div>
-          password:
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value="testpassword"
-          />
-        </div>
-        <div>
-          <button onClick={handleAddUser}>adduser</button>
-        </div>
-      </form>
-    </>
-  );
-}
+import { listUser, processListUser } from "src/components/modals/user";
 
 export default function UserExample() {
+  let [users, setUsers] = React.useState([]);
+
+  function handleListUser(e) {
+    processListUser()
+      .then((a_d) => {
+        setUsers(a_d);
+      })
+      .catch((err) => {
+        alert("error during listing user", err);
+      });
+  }
+
+  function ListUser() {
+    return (
+      <>
+        <button onClick={handleListUser}>list user</button>
+        {users.map((user, idx) => {
+          return (
+            <div key={idx}>
+              <pre>{JSON.stringify(user, null, 2)}</pre>
+            </div>
+          );
+        })}
+      </>
+    );
+  }
+
+  function handleAddUser(e) {
+    e.preventDefault();
+    let target_form = document.forms["add-user-form"];
+    let username = target_form.username.value;
+    let password = target_form.password.value;
+    console.log("username", username);
+    processAddUser(username, password)
+      .then(() => {
+        alert("add user done");
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }
+
+  function AddUser() {
+    const handleDummy = () => {};
+    return (
+      <>
+        <form name="add-user-form">
+          <div>
+            username:
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value="testuser"
+              onChange={handleDummy}
+            />
+          </div>
+          <div>
+            password:
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value="testpassword"
+              onChange={handleDummy}
+            />
+          </div>
+          <div>
+            <button onClick={handleAddUser}>adduser</button>
+          </div>
+        </form>
+      </>
+    );
+  }
+
   return (
     <>
       <div>UserExample</div>
+      <div>list user</div>
+      <div>
+        <ListUser />
+      </div>
+      <div>add user</div>
       <div>
         <AddUser />
       </div>
--- react-firebase-crud/src/components/modals/user.js
+++ react-firebase-crud/src/components/modals/user.js
@@ -7,16 +7,43 @@ import firebase_app from "src/Firebase/config";
 const db = firebase_app.firestore();
 let test_collection_ref = db.collection("react-firebase-crud");
 
+const default_new_user_profile = {
+  setting1: false,
+  setting2: true,
+  setting3: false,
+};
+
 function listUser() {}
+
+function processListUser() {
+  let promise = new Promise(function (resolve, reject) {
+    test_collection_ref.onSnapshot(
+      (ss) => {
+        let docs = ss.docs;
+        let a_d = docs.map((doc) => doc.data());
+        resolve(a_d);
+      },
+      (err) => {
+        reject(err);
+      }
+    );
+  });
+  return promise;
+}
 
 function dbAddUser(username) {}
 
-function processAddUser({ username, password }) {
+function processAddUser(username, password) {
+  let user_to_add = {
+    ...default_new_user_profile,
+    username: username,
+    password: password,
+  };
+  console.log("user_to_add", user_to_add);
+
   let promise = new Promise(function (resolve, reject) {
     test_collection_ref
-      .add({
-        hello: "world",
-      })
+      .add(user_to_add)
       .then(() => {
         resolve("add user done");
       })
@@ -29,4 +56,4 @@ function processAddUser({ username, password }) {
 
 function updateUser() {}
 
-export { updateUser, listUser, processAddUser };
+export { updateUser, listUser, processAddUser, processListUser };
