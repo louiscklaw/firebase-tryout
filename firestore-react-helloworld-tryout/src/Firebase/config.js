05a84730e5c912e0027ce7f66d34cbbcd12fd7b3
louiscklaw <louiscklaw@gmail.com>
2021-03-01 15:06:30 +0800

    update,

--- firestore-react-helloworld-tryout/src/Firebase/config.js
+++ firestore-react-helloworld-tryout/src/Firebase/config.js
@@ -2,15 +2,26 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
+// const fb_config = {
+//   apiKey: "AIzaSyBnmrMAfoQvVRgXCIPG2wLiGO15s7u4bRY",
+//   authDomain: "fir-tryout-f4e7a.firebaseapp.com",
+//   databaseURL: "https://fir-tryout-f4e7a.firebaseio.com",
+//   projectId: "fir-tryout-f4e7a",
+//   storageBucket: "fir-tryout-f4e7a.appspot.com",
+//   messagingSenderId: "859668436628",
+//   appId: "1:859668436628:web:32fcc829845132db900681",
+//   measurementId: "G-Z001Y7HBM2",
+// };
+
 const fb_config = {
-  apiKey: "AIzaSyBnmrMAfoQvVRgXCIPG2wLiGO15s7u4bRY",
-  authDomain: "fir-tryout-f4e7a.firebaseapp.com",
-  databaseURL: "https://fir-tryout-f4e7a.firebaseio.com",
-  projectId: "fir-tryout-f4e7a",
-  storageBucket: "fir-tryout-f4e7a.appspot.com",
-  messagingSenderId: "859668436628",
-  appId: "1:859668436628:web:32fcc829845132db900681",
-  measurementId: "G-Z001Y7HBM2",
+  apiKey: "AIzaSyDK21LG5IV2ajFzVa1V0jZhAxR4ar37mrk",
+  authDomain: "lynked-demo-tryout.firebaseapp.com",
+  databaseURL: "https://lynked-demo-tryout-default-rtdb.firebaseio.com",
+  projectId: "lynked-demo-tryout",
+  storageBucket: "lynked-demo-tryout.appspot.com",
+  messagingSenderId: "335675372584",
+  appId: "1:335675372584:web:ab02b5849ff52bb5f1711d",
+  measurementId: "G-E4KY2LRKDF",
 };
 
 const app = firebase.initializeApp(fb_config);
--- firestore-react-helloworld-tryout/src/components/Helloworld/index.js
+++ firestore-react-helloworld-tryout/src/components/Helloworld/index.js
@@ -6,12 +6,19 @@ import db_config from "src/configs/db_config";
 export default function Example() {
   let [debug, setDebug] = React.useState();
   const handleLoadListClick = () => {
-    db.collection(db_config.DB_TABLE_USERS)
-      .doc(`0xNnfLmW1GVRc430hJrR`)
+    alert("start");
+    db.collection("meny_manage_all_food_details")
+      .doc(`large_menu_test`)
       .get()
       .then((doc) => {
         console.log(doc.data());
         setDebug(doc.data());
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        alert("done");
       });
   };
   return (
