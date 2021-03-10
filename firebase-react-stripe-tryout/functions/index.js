b38a47d21e326314614e806da626159bffb6ae23
louiscklaw <louiscklaw@gmail.com>
2021-03-10 15:55:50 +0800

    update successful build,

--- firebase-react-stripe-tryout/functions/index.js
+++ firebase-react-stripe-tryout/functions/index.js
@@ -1,11 +1,22 @@
+"use strict";
+
+const path = require("path");
 const functions = require("firebase-functions");
 
 const express = require("express");
 const cors = require("cors");
-var cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-// const test_app = express();
-// test_app.use(cookieParser());
+const { Stripe } = require("stripe");
+const stripe = new Stripe(functions.config().stripe.secret, {
+  apiVersion: "2020-08-27",
+});
+
+const card_pay = express();
+const main = express();
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
